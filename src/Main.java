c522a35930d5d9603ccabc9346c9ee7fb085c4c6
mobis <mobis@192.168.1.220>
2016-01-16 15:36:46 +0200

    4

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
+import java.applet.Applet;
+import java.awt.Button;
+import java.awt.HeadlessException;
+
+import javax.swing.JApplet;
+
 import mvc.Controller;
 import mvc.TreeView;
 import mvc.View;
 
 
-public class Main {
+public class Main extends JApplet {
+	
+
+	public Main() throws HeadlessException {
+		super();
+		// TODO Auto-generated constructor stub
+		setSize(640, 480);
+		setVisible(true);
+		add(new Button("jopa"));
+		System.out.println("asdfasfadsf");
+	}
 
 	public static void main(String[] args) {
-        Controller c = new Controller();
-        TreeView v = new TreeView(c);
+		new Main();
+        //Controller c = new Controller();
+        //TreeView v = new TreeView(c);
         //View vv = new View(c);
         //MainForm mf = new MainForm();
 	}
